9c1256520ba7d47545d47c17e98dc6a5aee49a73
Sam <103394368+sam-orevillestudios@users.noreply.github.com>
2022-05-24 12:28:02 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,5 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Sprintf("fork-test")
-	fmt.Sprintf("code change1")
+	fmt.Sprintf("code change")
+	fmt.Sprintf("Another code change")
+	fmt.Sprintf("upstream change")
 }
